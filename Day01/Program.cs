7d78f794b9244b4c7886a250a77bda4bb6e1a18d
Michał Dulski <dulski.michal@outlook.com>
2024-12-02 22:27:35 +0100

    feat: 1st and 2nd of December

---
+++ Day01/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = File.ReadAllLines("../../../input.txt");
+
+List<int> leftCol = [];
+List<int> rightCol = [];
+
+Dictionary<int, int> occurrencesLeft = new Dictionary<int, int>();
+Dictionary<int, int> occurrencesRight = new Dictionary<int, int>();
+
+var linesLength = input.Length;
+
+foreach (var line in input)
+{
+    var parts = line.Split("   ");
+    leftCol.Add(int.Parse(parts[0]));
+    rightCol.Add(int.Parse(parts[1]));
+    occurrencesLeft[int.Parse(parts[0])] = occurrencesLeft.ContainsKey(int.Parse(parts[0])) ? occurrencesLeft[int.Parse(parts[0])] + 1 : 1;
+    occurrencesRight[int.Parse(parts[1])] = occurrencesRight.ContainsKey(int.Parse(parts[1])) ? occurrencesRight[int.Parse(parts[1])] + 1 : 1;
+}
+
+leftCol.Sort();
+rightCol.Sort();
+
+var sum = 0;
+
+for(int i = 0; i < linesLength; i++)
+{
+    sum += Math.Abs(leftCol[i] - rightCol[i]);
+}
+
+Console.WriteLine(sum);
+
+var similarity = 0;
+foreach(var key in occurrencesLeft.Keys)
+{
+    if(occurrencesRight.TryGetValue(key, out var value))
+        similarity += occurrencesLeft[key] * key * value;
+}
+
+Console.WriteLine(similarity);
---
+++ Day02/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = File.ReadAllLines("../../../input.txt");
+
+var numOfSafeReports = 0;
+foreach (var line in input)
+{
+    var levels = line.Split(" ").Select(int.Parse).ToArray();
+    
+    bool isSafe = true;
+    bool isIncreasing = levels[0] < levels[1];
+    for(int i = 1; i < levels.Length; i++)
+    {
+        var prevElement = levels[i - 1];
+        var currentElement = levels[i];
+        
+        var diff = currentElement - prevElement;
+        if(isIncreasing && diff < 0)
+        {
+            isSafe = false;
+            break;
+        }
+        if(!isIncreasing && diff > 0)
+        {
+            isSafe = false;
+            break;
+        }
+        
+        if(Math.Abs(diff) > 3 || diff == 0)
+        {
+            isSafe = false;
+            break;
+        }
+    }
+    numOfSafeReports += isSafe ? 1 : 0;
+}
+
+Console.WriteLine(numOfSafeReports);
