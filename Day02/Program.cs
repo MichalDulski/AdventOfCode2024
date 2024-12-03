444e17810826d358cd3dd182ce04464ddc9b653e
Michał Dulski <dulski.michal@outlook.com>
2024-12-03 01:16:38 +0100

    feat: forced Day 2 part 2

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -1,38 +1,78 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var input = File.ReadAllLines("../../../input.txt");
-
-var numOfSafeReports = 0;
-foreach (var line in input)
+(bool, int) IsSafe(int[] levels)
 {
-    var levels = line.Split(" ").Select(int.Parse).ToArray();
-    
     bool isSafe = true;
     bool isIncreasing = levels[0] < levels[1];
-    for(int i = 1; i < levels.Length; i++)
+    int removeIndex = -1;
+    for (int i = 1; i < levels.Length; i++)
     {
         var prevElement = levels[i - 1];
         var currentElement = levels[i];
-        
+
         var diff = currentElement - prevElement;
-        if(isIncreasing && diff < 0)
+        if (isIncreasing && diff < 0)
         {
             isSafe = false;
+            removeIndex = i;
             break;
         }
-        if(!isIncreasing && diff > 0)
+
+        if (!isIncreasing && diff > 0)
         {
             isSafe = false;
+            removeIndex = i;
             break;
         }
-        
-        if(Math.Abs(diff) > 3 || diff == 0)
+
+        if (Math.Abs(diff) > 3 || diff == 0)
         {
             isSafe = false;
+            removeIndex = i;
             break;
         }
     }
-    numOfSafeReports += isSafe ? 1 : 0;
+
+    return (isSafe, removeIndex);
 }
 
+var input = File.ReadAllLines("../../../input.txt");
+
+var numOfSafeReports = 0;
+var numOfSafeReportsDamp = 0;
+
+foreach (var line in input)
+{
+    var levels = line.Split(" ").Select(int.Parse).ToArray();
+    var (isSafe, removeIndex) = IsSafe(levels);
+    numOfSafeReports += isSafe ? 1 : 0;
+    if (!isSafe)
+    {
+        var newLevels = levels.ToList();
+        newLevels.RemoveAt(removeIndex);
+        var (isSafeDamp, _) = IsSafe(newLevels.ToArray());
+
+        if (!isSafeDamp)
+        {
+            var reversedLevels = levels.Reverse().ToList();
+            var (_, removeAt) = IsSafe(reversedLevels.ToArray());
+            var newLevels3 = reversedLevels.ToList();
+            newLevels3.RemoveAt(removeAt);
+            var (isSafeDamp3, _) = IsSafe(newLevels3.ToArray());
+            numOfSafeReportsDamp += isSafeDamp3 ? 1 : 0;
+        }
+        else
+        {
+            numOfSafeReportsDamp += 1;
+        }
+    }
+    else
+    {
+        numOfSafeReportsDamp += 1;
+    }
+}
+
+
 Console.WriteLine(numOfSafeReports);
+
+Console.WriteLine(numOfSafeReportsDamp);
